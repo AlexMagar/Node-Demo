803e24d58e26cf0a1010c61dd3d29729583c2199
AlexMagar <lx.mgr07@gmail.com>
2023-06-13 23:17:01 +1000

    route method

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 import express from "express";
+import router from "./src/router/taskRouter.js";
 
 //create an express variable
 const app = express();
 const PORT = 8000;
 // console.log("What comes in express: ",app)
 
+//morgan is to keep the log of the activities, login purpose
+
 // middlewares
 app.use(express.json());
 
 //this is to print in browser, it is a json file (API)
+app.use("/api/v1/task", router)
+
 app.use("/", (req, res) =>{
-    res.json({message: "server running healthy"})
+    res.json({message: "server runnung healthy"})
 })
 
 // open port for http request to access the server
